bcba0a33b6b62dc68ff8460ab444ef1761f670ae
xxxfffxxx <13509221776@163.com>
2026-05-18 10:59:41 +0800

    add swap function

--- test.c
+++ test.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+
+void swap (int *a,int *b ){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main()
 {
-    printf("hello world\n");
+    int a,b;
+    a=10;
+    b=20;
+        printf("before swap,a=%d,b=%d\n",a,b);
+    swap(&a,&b);
+        printf("after swpa,a=%d,b=%d\n",a,b);
+        printf("swap finish!\n");
     return 0;
 }
